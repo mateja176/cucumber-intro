eb7552ddcb0abe6712a23aa48106ee6c6349e8cf
Mateja Petrovic <mateja176@gmail.com>
2019-02-24 14:15:06 +0100

    Passes 2 out of 3 tests

--- features/step_definitions/stepdefs.js
+++ features/step_definitions/stepdefs.js
@@ -1,15 +1,19 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var cucumber_1 = require("cucumber");
-// ? Given today is Sunday
-// Undefined. Implement with the following snippet:
-// Write code here that turns the phrase above into concrete actions
-cucumber_1.Given("today is Sunday", function () { return "pending"; });
-// ? When I ask whether it's Friday yet
-// Undefined. Implement with the following snippet:
-// Write code here that turns the phrase above into concrete actions
-cucumber_1.When("I ask whether it's Friday yet", function () { return "pending"; });
-// ? Then I should be told "Nope"
-// Undefined. Implement with the following snippet:
-// Write code here that turns the phrase above into concrete actions
-cucumber_1.Then("I should be told {string}", function () { return "pending"; });
+const assert = require('assert');
+const { Given, When, Then } = require('cucumber');
+
+// eslint-disable-next-line
+function isItFriday(today) {
+  // We'll leave the implementation blank for now
+}
+
+Given('today is Sunday', function () {
+  this.today = 'Sunday';
+});
+
+When("I ask whether it's Friday yet", function () {
+  this.actualAnswer = isItFriday(this.today);
+});
+
+Then('I should be told {string}', function (expectedAnswer) {
+  assert.equal(this.actualAnswer, expectedAnswer);
+});
