c74b97ef20c5d69015eb1cc74cd110111e34b86f
Mateja Petrovic <mateja176@gmail.com>
2019-02-24 01:46:31 +0100

    Renames stepdefs.ts to stepdefs.js because cucumber does not support typescript out of the box

---
+++ features/step_definitions/stepdefs.js
@@ -0,0 +1,19 @@
+const { Given, Then, When } = require('cucumber');
+
+// ? Given today is Sunday
+// Undefined. Implement with the following snippet:
+
+// Write code here that turns the phrase above into concrete actions
+Given('today is Sunday', () => 'pending');
+
+// ? When I ask whether it's Friday yet
+// Undefined. Implement with the following snippet:
+
+// Write code here that turns the phrase above into concrete actions
+When("I ask whether it's Friday yet", () => 'pending');
+
+// ? Then I should be told "Nope"
+// Undefined. Implement with the following snippet:
+
+// Write code here that turns the phrase above into concrete actions
+Then('I should be told {string}', () => 'pending');
