7fae494516da20662ca127b0ad0666e0c8450bbc
Mateja Petrovic <mateja176@gmail.com>
2019-02-24 01:54:01 +0100

    Adds stepdefs.ts which is compiled before running cucumber

--- features/step_definitions/stepdefs.ts
+++ features/step_definitions/stepdefs.ts
@@ -1,15 +1,19 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var cucumber_1 = require("cucumber");
+import { Given, Then, When } from "cucumber"
+
 // ? Given today is Sunday
 // Undefined. Implement with the following snippet:
+
 // Write code here that turns the phrase above into concrete actions
-cucumber_1.Given("today is Sunday", function () { return "pending"; });
+Given("today is Sunday", () => "pending")
+
 // ? When I ask whether it's Friday yet
 // Undefined. Implement with the following snippet:
+
 // Write code here that turns the phrase above into concrete actions
-cucumber_1.When("I ask whether it's Friday yet", function () { return "pending"; });
+When("I ask whether it's Friday yet", () => "pending")
+
 // ? Then I should be told "Nope"
 // Undefined. Implement with the following snippet:
+
 // Write code here that turns the phrase above into concrete actions
-cucumber_1.Then("I should be told {string}", function () { return "pending"; });
+Then("I should be told {string}", () => "pending")
